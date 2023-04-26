4a8f2377672605cc2764282ce3ab245f9dfab1e7
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2023-04-26 00:18:28 -0400

    feat: :sparkles: play around with framer-motion

--- pages/index.js
+++ pages/index.js
@@ -10,60 +10,108 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax"
 import { ParallaxBanner } from 'react-scroll-parallax'
 
 export default function Home() {
-
   const isOpen = true;
 
-  const variants = {
-    open: { x: 0 },
-    closed: { x: "-100%" }
-  }
+  const draw = {
+    hidden: { pathLength: 0, opacity: 0 },
+    visible: (i) => {
+      const delay = 1 + i * 0.5;
+      return {
+        pathLength: 1,
+        opacity: 1,
+        transition: {
+          pathLength: { delay, type: "spring", duration: 1.5, bounce: 0 },
+          opacity: { delay, duration: 0.01 }
+        }
+      };
+    }
+  };
 
   return (
-    // <Parallax pages={1.75}>
-        // <ParallaxLayer offset={0.75} speed={0.5} size={0.25}>
-          // <img src="/MLH.png" height="100" className={styles.mlh}/>
-        // </ParallaxLayer>
-        // <ParallaxLayer offset={0} speed={0} size={2} pages>
-        <>
-          <Navbar/>
-          <motion.div className={styles.flex}
-            animate={isOpen ? "open" : "closed"}
-            variants={variants}
-            transition={{stiffness: 100}}
-          >
-            <div className={styles.title}>
-              <h1 className={styles.titleText}>
-                Hack BRHS 2023
-              </h1>
-              <p className={styles.paragraph}>
-                Hack BRHS is an event taking place in 2023. It will have coding and many people will be involved.
-              </p>
-            </div>
-            <div className={styles.titleSecondary}>
-              <h1 className={styles.titleText}>
-                FAQ
-              </h1>
-              <div className={styles.dark}>
-                <Question title="How do I register?">
-                    Just Sign Up!
-                </Question>
-                <Question title="How do I get good at coding?">
-                    Just improve your skills and learn from youtube
-                </Question>
-                <Question title="Should I join CWAP?">
-                    Yes of Course!
-                </Question>
-                <Question title="What kind of workshops are there?">
-                    There are a large variety of workshops.
-                </Question>
-              </div>
-            </div>
-            <Schedule/>
-            <Team/>
-            <Registration title="Registration" desc="Pls register"/>
-          </motion.div>
-        {/* </ParallaxLayer> */}
-    {/* </Parallax> */}
+    <>
+      <Navbar/>
+      <div className={styles.svg}>
+        <motion.svg
+        width="400"
+        height="400"
+        viewBox="0 0 600 600"
+        initial="hidden"
+        animate="visible"
+        >
+          <motion.circle
+            cx="100"
+            cy="100"
+            r="80"
+            stroke="#ff0055"
+            variants={draw}
+            custom={1}
+            className={styles.circle}
+          />
+          <motion.line
+            x1="220"
+            y1="30"
+            x2="360"
+            y2="170"
+            stroke="#00cc88"
+            variants={draw}
+            custom={2}
+            className={styles.line}
+          />
+          <motion.line
+            x1="220"
+            y1="170"
+            x2="360"
+            y2="30"
+            stroke="#00cc88"
+            variants={draw}
+            custom={2.5}
+            className={styles.line}
+          />
+          <motion.rect
+            width="140"
+            height="140"
+            x="410"
+            y="30"
+            rx="20"
+            stroke="#0099ff"
+            variants={draw}
+            custom={3}
+            className={styles.rect}
+          />
+        </motion.svg>
+      </div>
+      {/* <div> */}
+        <div className={styles.title}>
+          <h1 className={styles.titleText}>
+            Hack BRHS 2023
+          </h1>
+          <p className={styles.paragraph}>
+            Hack BRHS is an event taking place in 2023. It will have coding and many people will be involved.
+          </p>
+        </div>
+        <div className={styles.titleSecondary}>
+          <h1 className={styles.titleText}>
+            FAQ
+          </h1>
+          <div className={styles.dark}>
+            <Question title="How do I register?">
+                Just Sign Up!
+            </Question>
+            <Question title="How do I get good at coding?">
+                Just improve your skills and learn from youtube
+            </Question>
+            <Question title="Should I join CWAP?">
+                Yes of Course!
+            </Question>
+            <Question title="What kind of workshops are there?">
+                There are a large variety of workshops.
+            </Question>
+          </div>
+        </div>
+        <Schedule/>
+        <Team/>
+        <Registration title="Registration" desc="Pls register"/>
+      {/* </div> */}
     </>
   )
 }
