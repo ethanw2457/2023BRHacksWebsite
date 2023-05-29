71361c098cf4f35d2cccca4fa8f64ffaf6dda05d
Kinshuk Goel <67846964+Kinshuk-Goel@users.noreply.github.com>
2023-05-28 23:04:33 -0400

    Updates to `Sponsors` section
        Modified files:
        - Sponsors.js
        - Sponsors.module.scss
    
        - Animation updates
        - Background updates

--- comps/Sponsors.js
+++ comps/Sponsors.js
@@ -1,28 +1,103 @@
 import styles from './Sponsors.module.scss';
-import { motion } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect, useRef } from 'react';
 
 
 const Sponsors = () => {
+    const sponsorsRef = useRef(null);
+    const controls = useAnimation();
+  
+    useEffect(() => {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          const entry = entries[0];
+          if (entry.isIntersecting) {
+            controls.start('visible');
+          } else {
+            controls.start('hidden');
+          }
+        },
+        {
+          threshold: 1, 
+        }
+      );
+  
+      if (sponsorsRef.current) {
+        observer.observe(sponsorsRef.current);
+      }
+  
+      return () => {
+        if (sponsorsRef.current) {
+          observer.unobserve(sponsorsRef.current);
+        }
+      };
+    }, []);
+  
     return (
-        <>
-            <h1 className={styles.sponsorsTitle}>
-                Sponsors
-            </h1>
-            <div className={styles.displayBox}>
-                <img width="300px"src="Qualcomm-Logo.png" className={styles.logo}>
-                    {/* <span className={styles.logoWrappr}/> */}
-                </img>
-                {/* <span className={styles.logoWrapper}> */}
-                    <img src="MLH.png" width="300px" className={styles.logo}/>
-                {/* </span> */}
-            </div>
-            <div className={styles.displayBox}>
-                {/* <span className={styles.logoWrapper}> */}
-                <img src="medium.png" className={styles.logo}/>
-                {/* </span> */}
-            </div>
-        </>
-    )
+      <>
+        <h1 className={styles.sponsorsTitle}>
+          Sponsors
+        </h1>
+        <div className={`${styles.displayBox} ${styles.sponsorsSection}`} ref={sponsorsRef}>
+          <motion.div className={styles.logoWrapper}>
+            <motion.a
+              href="https://www.qualcomm.com/"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <motion.img
+                src="Qualcomm-Logo.png"
+                width="300px"
+                className={styles.logo}
+                initial="hidden"
+                animate={controls}
+                variants={{
+                  hidden: { opacity: 0, y: -50 },
+                  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+                }}
+              />
+            </motion.a>
+          </motion.div>
+          <motion.div className={styles.logoWrapper}>
+            <motion.a
+              href="https://mlh.io/"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <motion.img
+                src="MLH.png"
+                width="300px"
+                className={styles.logo}
+                initial="hidden"
+                animate={controls}
+                variants={{
+                  hidden: { opacity: 0, y: -50 },
+                  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+                }}
+              />
+            </motion.a>
+          </motion.div>
+          <motion.div className={styles.logoWrapper}>
+            <motion.a
+              href="https://womeninstem.org/"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <motion.img
+                src="medium.png"
+                className={styles.logo}
+                initial="hidden"
+                animate={controls}
+                variants={{
+                  hidden: { opacity: 0, y: -50 },
+                  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+                }}
+              />
+            </motion.a>
+          </motion.div>
+        </div>
+      </>
+    );
 }
 
 export default Sponsors;
