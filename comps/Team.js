a9a5ebf038bdfe35fd05c095fef805be843516ad
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2023-05-29 15:59:50 -0400

    chore: :bug: fix buggy mobile ui

--- comps/Team.js
+++ comps/Team.js
@@ -2,6 +2,7 @@ import styles from './TeamDesc.module.scss'
 import TeamDesc from './TeamDesc';
 import LRButton from './LRButton';
 import { useEffect, useState } from 'react';
+import { set } from 'immutable';
 
 const tech = [
   {
@@ -58,6 +59,7 @@ const team = [
 const Team = () => {
 
     const [cycle, setCycle] = useState(0);
+    const [isMobile, setIsMobile] = useState(false);
     const toggleUp = () => {
       setCycle((cycle + team.length - 1) % team.length);
     }
@@ -66,6 +68,14 @@ const Team = () => {
       setCycle((cycle + 1) % team.length);
     }
 
+    useEffect(() => {
+      if (window.matchMedia('(max-width: 600px)').matches) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    }, [])
+
     return (
         <div className={styles.teamTitle} id="team">
           <h1 className={styles.titleText}>
@@ -73,15 +83,29 @@ const Team = () => {
           </h1>
           <div className={styles.container}>
             <LRButton onClick={toggleDown} left/>
-            {/* <div className={styles.card}> */}
-            {team[cycle].map((el) => {
-              return (
-                <>
-                <TeamDesc name={el.name} desc={el.role} src={el.src} key={el.name}/>
-                </>
-              )
-            })}
-            {/* </div> */}
+                {isMobile && <div className={styles.teamWrapper}>
+                  {team[cycle].map((el) => {
+                    return (
+                      <>
+                      <TeamDesc name={el.name} desc={el.role} src={el.src} key={el.name}/>
+                      </>
+                    )
+                  })}
+                </div>}
+                
+                {!isMobile && 
+                  ( <>
+                    {team[cycle].map((el) => {
+                    return (
+                      <>
+                      <TeamDesc name={el.name} desc={el.role} src={el.src} key={el.name}/>
+                      </>
+                    )
+                  })}
+                  </>
+                  )
+                }
+                {/* TODO: Find a better way to do this */}
             <LRButton onClick={toggleUp} right/>
           </div>
       </div>
