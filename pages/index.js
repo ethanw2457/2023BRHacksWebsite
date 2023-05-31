d29429d3cc82b044805d186428510635be0ff192
Alan Jiang <74510449+Oajoeawfjo-Jioa@users.noreply.github.com>
2023-05-31 02:45:27 -0400

    feat: :sparkles: visual changes

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ import { RegisterButton } from '../comps/RegisterButton'
 import { Parallax, ParallaxLayer } from "@react-spring/parallax"
 import { ParallaxBanner } from 'react-scroll-parallax'
 import { useState, useEffect } from 'react'
+import { useScroll } from 'framer-motion'
 import Down from '../comps/Down'
 
 const variants = {
@@ -26,8 +27,8 @@ const centerVariants = {
 export default function Home() {
   // const isOpen = true;
   const [isOpen, setIsOpen] = useState(false);
-
   const [doc, setDoc] = useState(null);
+  const [isMobile, setIsMobile] = useState(false);
 
   const scroll = (id) => {
       if (doc) {
@@ -59,6 +60,11 @@ export default function Home() {
 
   useEffect(() => {
       setDoc(document);
+      if (window.matchMedia('(max-width: 1000px)').matches) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
   }, [])
 
   const draw = {
@@ -75,7 +81,6 @@ export default function Home() {
       };
     }
   };
-
 
   return (
     <motion.nav
@@ -113,73 +118,102 @@ export default function Home() {
               <h1 className={styles.frontTitle}>
                 Hack BRHS
               </h1>
+              <img width="100%px" src="3161-removebg.png" className={styles.building}/>
               <img src="/Layer1.svg" className={styles.l1}/>
               <img src="/Layer2.svg" className={styles.l2}/>
               <img src="/Layer3.svg" className={styles.l3}/>
               <img src="/Layer4.svg" className={styles.l4}/>
+              <p className={styles.credit}>
+                <a href="https://www.freepik.com/free-vector/metropolis-night-landscape-neon-cartoon-vector_4393824.htm#query=cyberpunk%20city&position=0&from_view=keyword&track=ais">Image by vectorpocket</a> on Freepik
+              </p>
             </header>
           {/* </div> */}
         {/* <div> */}
-        <section className={styles.content} id="about">
-          <div className={styles.title}>
-            <h1 className={styles.titleText}>
-              Hack BRHS 2023
-            </h1>
-            <p className={styles.paragraph}>
-              Hack BRHS is an event taking place in 2023. It will have coding and many people will be involved.
-            </p>
-            <RegisterButton/>
-          </div>
-        </section>
-        <section className={styles.secondaryContent}>
-          <div className={styles.titleSecondary} id="faq">
-            <h1 className={styles.faq}>
-              FAQ
-            </h1>
-            <div className={styles.dark}>
-              <Question title="How do I register?">
-                  Just Sign Up!
-              </Question>
-              <Question title="How do I get good at coding?">
-                  Just improve your skills and learn from youtube
-              </Question>
-              <Question title="Should I join CWAP?">
-                  Yes of Course!
-              </Question>
-              <Question title="What kind of workshops are there?" last>
-                  There are a large variety of workshops.
-              </Question>
+        <div className={styles.parallax}>
+          <section className={styles.content} id="about">
+            <div className={styles.title}>
+              <h1 className={styles.titleText}>
+                Hack BRHS 2023
+              </h1>
+              <p className={styles.paragraph}>
+                Hack BRHS is an event taking place in 2023. It will have coding and many people will be involved.
+              </p>
+              <RegisterButton/>
             </div>
-          </div>
-        </section>
-        <section className={styles.tertiaryContent}>
-          <Schedule/>
-        </section>
-        <section className={styles.secondaryContent}>
-          <Team/>
-        </section>
-        <section id="sponsors" className={styles.sponsors}> 
-          <Sponsors/>
-        </section>
-        {/* <section className={styles.sponsors}>
-              <img src="/LayerR1.svg" className={styles.l1}/>
-              <img src="/LayerR2.svg" className={styles.l2}/>
-              <img src="/LayerR3.svg" className={styles.l3}/>
-              <img src="/LayerR4.svg" className={styles.l4}/>
-              <section className={styles.bottomTitle}>
-                <h1>
-                  Sponsors
-                </h1>
-              </section>
-        </section> */}
-        {/* <section>
-          <h1>
-            Sponsors
-          </h1>
-        </section> */}
-              <Registration title="Questions?" desc="Pls register"/>
+          </section>
+          <section className={styles.secondaryContent}>
+            <div className={styles.titleSecondary} id="faq">
+              <h1 className={styles.faq}>
+                FAQ
+              </h1>
+              <div className={styles.dark}>
+                <Question title="How do I register?">
+                    Just Sign Up!
+                </Question>
+                <Question title="How do I get good at coding?">
+                    Just improve your skills and learn from youtube
+                </Question>
+                <Question title="Should I join CWAP?">
+                    Yes of Course!
+                </Question>
+                <Question title="What kind of workshops are there?" last>
+                    There are a large variety of workshops.
+                </Question>
+              </div>
+            </div>
+          </section>
+          <section className={styles.tertiaryContent}>
+            <Schedule/>
+          </section>
+          <section className={styles.secondaryContent}>
+            <Team/>
+          </section>
 
-        {/* </div> */}
+          {/* <section className={styles.sponsors}>
+                <img src="/LayerR1.svg" className={styles.l1}/>
+                <img src="/LayerR2.svg" className={styles.l2}/>
+                <img src="/LayerR3.svg" className={styles.l3}/>
+                <img src="/LayerR4.svg" className={styles.l4}/>
+                <section className={styles.bottomTitle}>
+                  <h1>
+                    Sponsors
+                  </h1>
+                </section>
+          </section> */}
+          {/* <section>
+            <h1>
+              Sponsors
+            </h1>
+          </section> */}
+
+          {/* </div> */}
+
+          {!isMobile &&
+            <>
+            <motion.img width="200px" 
+                src="planet1.png"
+                className={styles.planet1}
+                id="planet1"
+            />
+            <motion.img width="200px" 
+              src="planet3.png" 
+              className={styles.planet3} 
+              id="planet3"
+            />
+            <motion.img width="200px" 
+              src="planet2.png" 
+              className={styles.planet2} 
+            />
+            <img width="200px" 
+              src="planet4.png" 
+              className={styles.planet4}/>
+          </>
+          }
+        </div>
+        <section id="sponsors" className={styles.sponsors}> 
+            <Sponsors/>
+        </section>
+        <Registration title="Questions?" desc="Pls register"/>
       </motion.div>
     </motion.nav>
   )
