340c76b2f090156b1aad8011358b84f8d8e88069
Kinshuk-Goel <kg467@outlook.com>
2023-08-22 23:14:11 -0400

    Massive changes to FAQ section

--- comps/Question.js
+++ comps/Question.js
@@ -1,54 +1,56 @@
-// Question.js 
+// Question.js
 
-import styles from './comps.module.scss';
+import styles from './Question.module.scss';
 import { useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
+
+import DownArrowIcon from '/public/whiteDownArrow.png'; 
+
 
 const variants = {
-    closed: {
-        opacity: 1
-    },
-    open: {
-        opacity: 0
-    }
-}
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+};
 
-const Question = ({title, last, children}) => {
+const Question = ({ title, last, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const toggleOpen = () => {
-        setIsOpen(!isOpen);
-    }
-    
-    const isLast = (el) => {
-        let style = last ? styles.lastQuestion : styles.questions;
-
-        return (
-            <div className={style}>
-                {el}
-            </div>
-        )
-    }
+  const isLast = (el) => {
+    let style = last ? styles.lastQuestion : styles.questions;
 
     return (
-        isLast(
-            <>
-                <h3 className={styles.question} onClick={toggleOpen}>
-                    {title}
-                </h3>
-                {isOpen && <motion.p className={styles.answer}
-                    variants={variants}
-                    initial={{opacity: 0}}
-                    animate={{opacity: 1}}
-                    transition={{stiffness: 100, delay: 0.1}}
-                >
-                    {children}
-                </motion.p>
-                }
-            </>
-        )
+      <div className={style} onClick={toggleOpen}>
+        {el}
+      </div>
     );
+  };
+
+  return (
+    isLast(
+      <>
+        <h3 className={styles.question}>{title}</h3>
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              className={styles.answerWrapper}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              variants={variants}
+              transition={{ duration: 0.3 }} // Adjust the transition duration as needed
+            >
+              <motion.p className={styles.answer}>{children}</motion.p>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </>
+    )
+  );
 }
 
 export default Question;
