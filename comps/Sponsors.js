5071e1120a25ad734a1cd1cadc4ef118242248f3
Kinshuk-Goel <kg467@outlook.com>
2023-08-29 22:58:03 -0400

    Sponsors update

--- comps/Sponsors.js
+++ comps/Sponsors.js
@@ -42,6 +42,8 @@ const Sponsors = () => {
   return (
     <div className={styles.sponsorsWrapper}>
       <h1 className={styles.sponsorsTitle}>Sponsors</h1>
+
+
       <div className={`${styles.displayBox} ${styles.sponsorsSection}`} ref={sponsorsRef}>
         {/* Qualcomn */}
         <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
@@ -171,8 +173,328 @@ const Sponsors = () => {
       </div>
 
 
+      <div className={`${styles.displayBox} ${styles.sponsorsSection}`} ref={sponsorsRef}>
+        {/* Interview Cake */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://www.interviewcake.com/"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <motion.img
+              src="interview+cake.png"
+              width={`${300 * scale}px`}
+              className={styles.logo}
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+        {/* Taskade */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://www.taskade.com/"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <motion.img
+              src="taskade.png"
+              className={styles.logo}
+              initial="hidden"
+              width={`${300 * scale}px`}
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+        {/* Sticker Giant */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://www.stickergiant.com/"
+            whileHover={{ scale: 1.1 }} // You can adjust the hover scale effect if needed
+            whileTap={{ scale: 0.9 }} // You can adjust the tap scale effect if needed
+          >
+            <motion.img
+              src="stickerGiant.webp" // Replace with the actual image filename of the echo3D logo
+              width={`${300 * scale}px`}
+              className={styles.logo}
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+      </div>
+
 
       <div className={`${styles.displayBox} ${styles.sponsorsSection}`} ref={sponsorsRef}>
+        {/* Axure */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://www.axure.com/"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <motion.img
+              src="axure.svg"
+              width={`${300 * scale}px`}
+              className={styles.logo}
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+        {/* Wolfram */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://www.wolfram.com/"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <motion.img
+              src="wolfram.png"
+              className={styles.logo}
+              initial="hidden"
+              width={`${300 * scale}px`}
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+        {/* Coding Ninjas Bridgewater */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://www.codeninjas.com/nj-bridgewater"
+            whileHover={{ scale: 1.1 }} // You can adjust the hover scale effect if needed
+            whileTap={{ scale: 0.9 }} // You can adjust the tap scale effect if needed
+          >
+            <motion.img
+              src="cNinjas.jpeg" // Replace with the actual image filename of the echo3D logo
+              width={`${300 * scale}px`}
+              className={styles.logo}
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+      </div>
+
+
+      <div className={`${styles.displayBox} ${styles.sponsorsSection}`} ref={sponsorsRef}>
+        {/* Zoned */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://zonedinc.com/"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <motion.img
+              src="zoned.png"
+              width={`${300 * scale}px`}
+              className={styles.logo}
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+        {/* Carneigie Evaluations */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://carnegieevaluations.com/"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <motion.img
+              src="careval.png"
+              className={styles.logo}
+              initial="hidden"
+              width={`${300 * scale}px`}
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+        {/* April Ludwig Photography */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://www.aprilludwig.com/"
+            whileHover={{ scale: 1.1 }} // You can adjust the hover scale effect if needed
+            whileTap={{ scale: 0.9 }} // You can adjust the tap scale effect if needed
+          >
+            <motion.img
+              src="alp.jpeg" // Replace with the actual image filename of the echo3D logo
+              width={`${300 * scale}px`}
+              className={styles.logo}
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+      </div>
+
+
+      <div className={`${styles.displayBox} ${styles.sponsorsSection}`} ref={sponsorsRef}>
+        {/* FSTONE */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://www.fstonetechnologies.com/"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <motion.img
+              src="fstone.png"
+              width={`${300 * scale}px`}
+              className={styles.logo}
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+        {/* Gold's Gym */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://www.goldsgym.com/"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <motion.img
+              src="golds.png"
+              className={styles.logo}
+              initial="hidden"
+              width={`${300 * scale}px`}
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+        {/* Mathnasium */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://www.mathnasium.com/"
+            whileHover={{ scale: 1.1 }} // You can adjust the hover scale effect if needed
+            whileTap={{ scale: 0.9 }} // You can adjust the tap scale effect if needed
+          >
+            <motion.img
+              src="maths.jpeg" // Replace with the actual image filename of the echo3D logo
+              width={`${300 * scale}px`}
+              className={styles.logo}
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+      </div>
+
+
+      <div className={`${styles.displayBox} ${styles.sponsorsSection}`} ref={sponsorsRef}>
+        {/* Carousel of Flowers NJ */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://www.carouselflowersnj.com/"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <motion.img
+              src="cof.png"
+              width={`${300 * scale}px`}
+              className={styles.logo}
+              initial="hidden"
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+        {/* Jetbrains */}
+        <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
+          <motion.a
+            href="https://www.jetbrains.com/"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <motion.img
+              src="jetbrains.webp"
+              className={styles.logo}
+              initial="hidden"
+              width={`${300 * scale}px`}
+              animate={controls}
+              variants={{
+                hidden: { opacity: 0, y: -50 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+            />
+          </motion.a>
+        </motion.div>
+
+        {/* Put Sponsor Here */}
+
+
+      </div>
+
+
+      <div className={`${styles.displayBox} ${styles.sponsorsSection}`} ref={sponsorsRef}>
+        {/* Put Sponsor Here */}
+
+
+
         {/* XYZ Domain */}
         <motion.div className={`${styles.logoWrapper} ${styles.card}`}>
           <motion.a
@@ -193,6 +515,26 @@ const Sponsors = () => {
             />
           </motion.a>
         </motion.div>
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* XYZ Domain -- and more things with it  */}
+
+      <div className={`${styles.displayBox} ${styles.sponsorsSection}`} ref={sponsorsRef}>
+
 
 
 
