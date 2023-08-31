5328ccb3b13b5b3c49a6e302ee9d3780f1898485
Kinshuk-Goel <kg467@outlook.com>
2023-08-31 14:15:50 -0400

    Team members update

--- comps/Team.js
+++ comps/Team.js
@@ -1,3 +1,5 @@
+// Team.js
+
 import styles from './TeamDesc.module.scss'
 import TeamDesc from './TeamDesc';
 import LRButton from './LRButton';
@@ -6,110 +8,240 @@ import { set } from 'immutable';
 
 const tech = [
   {
-    name: "Ethan",
+    name: "Ethan Wang",
     role: "President",
     src: ""
   },
   {
-    name: "Kinshuk",
-    role: "Tech",
+    name: "Vanshika Y",
+    role: "Finance Team Leader",
     src: ""
   },
   {
-    name: "Alan",
-    role: "Tech",
+    name: "Ceiron D",
+    role: "Events & Logistics Team Leader",
     src: ""
   },
   {
-    name: "Andrew",
-    role: "Tech",
+    name: "Chris Q",
+    role: "Events & Logistics Team Leader",
+    src: ""
+  },
+  {
+    name: "Saanvi G",
+    role: "Outreach Team Leader",
+    src: ""
+  },
+  {
+    name: "Jessica K",
+    role: "Social Media Team Leader",
+    src: ""
+  },
+  {
+    name: "Kinshuk G",
+    role: "Tech & Design Team Leader",
+    src: ""
+  }
+];
+
+const finance = [
+
+  {
+    name: "Vraj S",
+    role: "Finance Team",
+    src: ""
+  },
+  {
+    name: "Krish",
+    role: "Finance Team",
+    src: ""
+  },
+  {
+    name: "Nikita K",
+    role: "Finance Team",
+    src: ""
+  },
+  {
+    name: "Ridhi P",
+    role: "Finance Team",
+    src: ""
+  },
+  {
+    name: "Divyansha N",
+    role: "Finance Team",
+    src: ""
+  },
+  {
+    name: "Shreya G",
+    role: "Finance Team",
+    src: ""
+  }
+];
+
+const events_logistics = [
+
+  {
+    name: "Andrei C",
+    role: "Events and Logistics Team",
+    src: ""
+  },
+  {
+    name: "Nikita S",
+    role: "Events and Logistics Team",
+    src: ""
+  },
+  {
+    name: "Arnav D",
+    role: "Events and Logistics Team",
+    src: ""
+  },
+  {
+    name: "Aarav N",
+    role: "Events and Logistics Team",
+    src: ""
+  },
+  {
+    name: "Pranav S",
+    role: "Events and Logistics Team",
     src: ""
   }
 ];
 
 const outreach = [
+
   {
-    name: "Other",
-    role: "President",
+    name: "Joon D",
+    role: "Outreach Team",
     src: ""
   },
   {
-    name: "Other",
-    role: "Tech",
+    name: "Soham C",
+    role: "Outreach Team",
     src: ""
   },
   {
-    name: "Other",
-    role: "Tech",
+    name: "Nakul R",
+    role: "Outreach Team",
     src: ""
   },
   {
-    name: "Other",
-    role: "Tech",
+    name: "Advik J",
+    role: "Outreach Team",
+    src: ""
+  },
+  {
+    name: "Kevin A",
+    role: "Outreach Team",
     src: ""
   }
 ];
 
+const social_media = [
 
-const team = [
+  {
+    name: "Amogh J",
+    role: "Social Media Team",
+    src: ""
+  },
+  {
+    name: "Victoria W",
+    role: "Social Media Team",
+    src: ""
+  },
+  {
+    name: "Afraz A",
+    role: "Social Media Team",
+    src: ""
+  },
+  {
+    name: "Harshini Y",
+    role: "Social Media Team",
+    src: ""
+  }
+];
+
+const tech_and_design = [
+
+  {
+    name: "Alan J",
+    role: "Tech and Design Team",
+    src: ""
+  },
+  {
+    name: "Luvya M",
+    role: "Tech and Design Team",
+    src: ""
+  },
+  {
+    name: "Andrew Z",
+    role: "Tech and Design Team",
+    src: ""
+  }
+];
+
+const teams = [
   tech,
-  outreach
-]
+  outreach,
+  finance,
+  events_logistics,
+  social_media,
+  tech_and_design
+];
 
 const Team = () => {
 
-    const [cycle, setCycle] = useState(0);
-    const [isMobile, setIsMobile] = useState(false);
-    const toggleUp = () => {
-      setCycle((cycle + team.length - 1) % team.length);
+  const [cycle, setCycle] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
+  const toggleUp = () => {
+    setCycle((cycle + teams.length - 1) % teams.length);
+  }
+
+  const toggleDown = () => {
+    setCycle((cycle + 1) % teams.length);
+  }
+
+  useEffect(() => {
+    if (window.matchMedia('(max-width: 600px)').matches) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
     }
+  }, [])
 
-    const toggleDown = () => {
-      setCycle((cycle + 1) % team.length);
-    }
-
-    useEffect(() => {
-      if (window.matchMedia('(max-width: 600px)').matches) {
-        setIsMobile(true);
-      } else {
-        setIsMobile(false);
-      }
-    }, [])
-
-    return (
-        <div className={styles.teamTitle} id="team">
-          <h1 className={styles.titleText}>
-            Team
-          </h1>
-          <div className={styles.container}>
-            <LRButton onClick={toggleDown} left/>
-                {isMobile && <div className={styles.teamWrapper}>
-                  {team[cycle].map((el) => {
-                    return (
-                      <>
-                      <TeamDesc name={el.name} desc={el.role} src={el.src} key={el.name}/>
-                      </>
-                    )
-                  })}
-                </div>}
-                
-                {!isMobile && 
-                  ( <>
-                    {team[cycle].map((el) => {
-                    return (
-                      <>
-                      <TeamDesc name={el.name} desc={el.role} src={el.src} key={el.name}/>
-                      </>
-                    )
-                  })}
+  return (
+      <div className={styles.teamTitle} id="team">
+        <h1 className={styles.titleText}>
+          Team
+        </h1>
+        <div className={styles.container}>
+          <LRButton onClick={toggleDown} left/>
+            {isMobile && <div className={styles.teamWrapper}>
+              {teams[cycle].map((el) => {
+                return (
+                  <>
+                  <TeamDesc name={el.name} desc={el.role} src={el.src} key={el.name}/>
                   </>
-                  )
-                }
-                {/* TODO: Find a better way to do this */}
-            <LRButton onClick={toggleUp} right/>
-          </div>
+                )
+              })}
+            </div>}
+            
+            {!isMobile && 
+              ( <>
+                {teams[cycle].map((el) => {
+                return (
+                  <>
+                  <TeamDesc name={el.name} desc={el.role} src={el.src} key={el.name}/>
+                  </>
+                )
+              })}
+              </>
+              )
+            }
+            {/* TODO: Find a better way to do this */}
+          <LRButton onClick={toggleUp} right/>
+        </div>
       </div>
-    );
+  );
 }
 
 export default Team;
