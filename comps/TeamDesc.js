924a9fae1b70c326062c96a3eb313afb6d689251
Kinshuk Goel <67846964+Kinshuk-Goel@users.noreply.github.com>
2023-09-09 21:58:02 -0400

    team members shape updated

--- comps/TeamDesc.js
+++ comps/TeamDesc.js
@@ -3,23 +3,31 @@
 import styles from './TeamDesc.module.scss'
 import { motion } from 'framer-motion'
 
-const TeamDesc = ({name, desc, src}) => {
-    return (
-        <motion.div className={styles.card}
-            initial={{opacity: 0}}
-            animate={{opacity: [0, 0.25, 0.25, 0.5, 0.5, 0.75, 0.75, 1]}}
-            transition={{stiffness: 100, delay: 0.25}}
-        >
-            {src != "" && <img width="100" src={src}/>}
-            {src == "" && <img width="100" src="/PFP_EmptyAvatar.png"/>}
-            <h3 className={styles.name}>
-                {name}
-            </h3>
-            <p className={styles.desc}>
-                {desc}
-            </p>
-        </motion.div>
-    )
+const TeamDesc = ({ name, desc, src }) => {
+  return (
+    <motion.div
+      className={styles.card}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: [0, 0.25, 0.25, 0.5, 0.5, 0.75, 0.75, 1] }}
+      transition={{ stiffness: 100, delay: 0.25 }}
+    >
+      {src !== "" ? (
+        <img
+          className={styles.teamPicture}
+          src={src}
+          alt={name}
+        />
+      ) : (
+        <img
+          className={styles.teamPicture}
+          src="/PFP_EmptyAvatar.png"
+          alt={name}
+        />
+      )}
+      <h3 className={styles.name}>{name}</h3>
+      <p className={styles.desc}>{desc}</p>
+    </motion.div>
+  )
 }
 
 export default TeamDesc;
