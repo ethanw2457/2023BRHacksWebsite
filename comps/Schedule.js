d12667ec9a399b1ac9e1fbb0d2e3ffd89cca3eee
Kinshuk Goel <67846964+Kinshuk-Goel@users.noreply.github.com>
2023-09-18 15:20:56 -0400

    schedule updates

--- comps/Schedule.js
+++ comps/Schedule.js
@@ -9,27 +9,26 @@ import styles from './Schedule.module.scss';
 
 
 const csvData = `Time,Event,Location
-Time,Event,Location
 9:00 AM,Check In Opens,
 9:30 AM,,
 10:00 AM,Opening Ceremony,Auditorium
-10:30 AM,Team Bonding During Opening Ceremony,Large Cafeteria
+10:30 AM,Team Bonding During Opening Ceremony,Large Cafetria
 11:15 AM,Intro Python,171
 11:15 AM,Intro to C++,172
 12:00 PM,Lunch - Chess Tournament Sign ups,Large Cafeteria
 12:30 PM,Lunch/Chess Tournament,
-1:00 PM,BR Prints (3D Printing Club),Small Cafeteria
-1:00 PM,AI/ML,173
+1:00 PM,3D Printing Workshop,Small Cafeteria - 2hrs
+1:00 PM,AI/Machine Learning Workshop,173
 1:45 PM,General CS Knowledge Competition,Large Cafeteria
 2:30 PM,Intro to Java,103
 2:30 PM,Will AI Be Able To Model Human Emotion?,173
-3:15 PM,Women In STEM,171
+3:15 PM,Cybersecurity Workshop,171
 3:15 PM,Intro to Data Structures and Algorithms,172
-4:00 PM,BR Engineering,173
+4:00 PM,Engineering Workshop,173
 4:00 PM,Neuroscience Workshop,103
-4:45 PM,Competitive Programming Competition,Small Cafeteria
+4:45 PM,Competitive Programming Competition,Small Cafetria
 5:45 PM,Game Dev Workshop,173
-5:45 PM,Cyber-security Workshop,103
+5:45 PM,Women In STEM,103
 6:30 PM,Data Visualization,172
 6:30 PM,Web Development and Github,171
 7:00 PM,Hacking Ends,
